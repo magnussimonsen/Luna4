b4c8492ce011cd81f020ae91a8a32678490d6654
Magnus Simonsen <61977949+magnussimonsen@users.noreply.github.com>
2025-07-07 18:44:51 +0200

    Add files via upload

---
+++ Luna-Electron-src/src/renderer/src/assets/aboutLuna.js
@@ -0,0 +1,19 @@
+// File: C:\Users\magnu\Luna\Luna4\src\renderer\src\assets\aboutLuna.js
+
+// Information about Luna
+
+export const aboutLuna = {
+  version: '0.1.0',
+  versionDate: '2021-10-01',
+  versionName: 'Luna',
+  license: 'Undecided',
+  webpage: 'www.luna4STEM.com',
+  github: 'none',
+  contributors: [
+    {
+      name: 'Magnus Simonsen',
+      email: 'magnus.storjord.simonsen@gmail.com',
+      role: 'Lead Developer'
+    }
+  ]
+}
